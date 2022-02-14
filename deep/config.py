173b5e7a4fa721dcf87f1e23808408ede59655e0
baesang0522@gmail.com <baesang@kcnet.co.kr>
2022-02-14 18:09:26 +0900

    k8s

--- deep/config.py
+++ deep/config.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 class Config:
-    TRAIN_PATH = './train_data/train.csv'
-    TEST_PATH = './test_data/test.csv'
+    TRAIN_PATH = 'train_data/train.csv'
+    TEST_PATH = 'test_data/test.csv'
     LABEL = './test_data/test.csv'
     DROPOUT_RATIO = 0.3
     BATCH_SIZE = 32
@@ -27,5 +27,5 @@ class ProdConfig(Config):
 config = {"DEV": DevConfig,
           "PROC": ProdConfig}
 
-data = pd.read_csv('./train_data/train.csv')
+data = pd.read_csv('train_data/train.csv')
 data = data.fillna(0)
